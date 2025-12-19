3a3c32a4694f0e2fb04b3641e30ff8428fec73ea
Milindh Vijay <milindhvijay@gmail.com>
2025-12-19 08:59:15 +0530

    Added configuration constants

---
+++ Sources/noSleep/Config.swift
@@ -0,0 +1,8 @@
+// Config.swift - Global constants
+
+import Foundation
+
+let VERSION = "1.0.0"
+let LABEL = "com.noSleep.daemon"
+let LOCKFILE = "/tmp/noSleep.lock"
+let PLIST_PATH = "\(NSHomeDirectory())/Library/LaunchAgents/\(LABEL).plist"
