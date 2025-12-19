0c61ece40a18e75f609ae6dfd1843c234940ea92
Milindh Vijay <milindhvijay@gmail.com>
2025-12-19 09:00:15 +0530

    Added IOPMAssertion wrapper for sleep prevention

---
+++ Sources/noSleep/SleepPreventer.swift
@@ -0,0 +1,29 @@
+// SleepPreventer.swift
+
+import Foundation
+import IOKit.pwr_mgt
+
+class SleepPreventer {
+    private var assertionID: IOPMAssertionID = 0
+    private(set) var isActive = false
+    
+    func preventSleep() {
+        guard !isActive else { return }
+        let result = IOPMAssertionCreateWithName(
+            kIOPMAssertPreventUserIdleSystemSleep as CFString,
+            IOPMAssertionLevel(kIOPMAssertionLevelOn),
+            "noSleep - lid closed on AC power" as CFString,
+            &assertionID
+        )
+        if result == kIOReturnSuccess {
+            isActive = true
+        }
+    }
+    
+    func allowSleep() {
+        guard isActive else { return }
+        IOPMAssertionRelease(assertionID)
+        isActive = false
+        assertionID = 0
+    }
+}
